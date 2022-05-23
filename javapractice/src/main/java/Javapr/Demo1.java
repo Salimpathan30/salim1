212fd3efbde4423e87de52436d192e4936e21b0e
MH SAURABH <MH SAURABH@DESKTOP-8EHF36N>
2022-05-23 14:51:42 +0530

    commited

---
+++ javapractice/src/main/java/Javapr/Demo1.java
@@ -0,0 +1,21 @@
+package Javapr;
+
+import java.util.Scanner;
+
+public class Demo1{
+	
+public static void main(String[] args) {
+	Scanner scan= new Scanner(System.in);
+	
+	System.out.println("Enter value of a: ");
+	int a=scan.nextInt();
+	System.out.println("Enter value of b: ");
+	
+	int b= scan.nextInt();
+	int c=a+b;
+	
+	System.out.println("addition of a and b is "+c);
+	
+
+}
+}
